241190a528768f090a95b8eff69f5ce4baa2254f
Steve Steinitz <steve@dtctc.com>
2016-01-27 17:06:18 +1100

    Shorten summary

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'steinitz:react-es6-autobind',
-  version: '0.1.0',
-  // Brief, one-line summary of the package.
-  summary: "For all methods, binds 'this' to the React component created with class extends. Without it 'this' refers to 'Window'.",
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/steinitz/Meteor-React-ES6-AutoBind',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'steinitz:react-es6-component',
+	version: '0.1.0',
+	// Brief, one-line summary of the package.
+	summary: 'Allow the use of getMeteorData in ES6 React classes similar to React.createClass class',
+	// URL to the Git repository containing the source code for this package.
+	git: '',
+	// By default, Meteor will default to using README.md for documentation.
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('react-es6-autobind.js');
-  api.export('SteinitzREA', ['client', 'server']);
+	api.versionsFrom('1.2.1');
+	api.use(['ecmascript', 'react@0.14.1_1', 'tracker', 'steinitz:react-es6-autobind@0.1.0']);
+	api.addFiles('react-es6-component.js');
+	api.export('SteinitzReactES6Component', ['client', 'server']);
+
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('steinitz:react-es6-autobind');
-  api.addFiles('react-es6-autobind-tests.js');
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('steinitz:react-es6-component');
+	api.addFiles('react-es6-component-tests.js');
 });
